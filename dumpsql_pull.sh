1d991cc12d2d44110da39918a8b42bc4ce638e26
xuxinlong <18170551225@163.com>
2018-06-09 20:43:43 +0800

    优化脚本

--- dumpsql_pull.sh
+++ dumpsql_pull.sh
@@ -3,16 +3,19 @@
 
 scp root@47.104.157.93:/data/app/service/blogs.sql blogs.sql 
 
+# mysql -uroot -proot
+
+# use blogs
+# 同步备份的数据
+# source /Users/xuxinlong/source/blogs-thinkjs/blogs.sql
+# exit
+
 #echo ' 服务器处理 '
 #ssh root@47.104.157.93 << eeooff
 	#cd /data/app/service
 	#mysqldump -u root -p blogs > blogs.sql
 	#需要手动输入密码
 
-	# mysql -uroot -p
-	# use blogs
-	# 同步备份的数据
-	# source /Users/xuxinlong/source/blogs-thinkjs/blogs.sql
 
 	#exit 
 #eeooff
